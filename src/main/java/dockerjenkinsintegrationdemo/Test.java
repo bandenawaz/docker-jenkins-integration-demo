d5e9f904599985be49465a9128082bef86e48ead
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-07-15 08:09:21 +0530

    Added TestClass with a messag

---
+++ src/main/java/dockerjenkinsintegrationdemo/Test.java
@@ -0,0 +1,12 @@
+package dockerjenkinsintegrationdemo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello from Jenkins Docker Integration");
+
+	}
+
+}
